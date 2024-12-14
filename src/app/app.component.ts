dab7050401e96d422b2877f653065b75128df4ff
Angelo Raso <araso@anura.com.ar>
2024-12-14 16:36:09 -0300

    Close popups with back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { BizyLogService, BizyRouterService, BizyToastService } from '@bizy/services';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  BizyLogService,
+  BizyPopupService,
+  BizyRouterService,
+  BizyToastService
+} from '@bizy/services';
 import { AuthService } from '@core/auth/auth.service';
 import { ROOT_PATHS } from '@core/constants';
 import { ERROR } from '@core/model';
@@ -12,64 +17,65 @@ import { PATH } from './app.routing';
   styleUrls: ['./app.css']
 })
 export class AppComponent implements OnInit {
-  constructor(
-    @Inject(MobileService) private mobile: MobileService,
-    @Inject(AuthService) private auth: AuthService,
-    @Inject(BizyLogService) private log: BizyLogService,
-    @Inject(BizyToastService) private toast: BizyToastService,
-    @Inject(DatabaseService) private database: DatabaseService,
-    @Inject(UsersService) private usersService: UsersService,
-    @Inject(BizyRouterService) private router: BizyRouterService
-  ) {}
+  readonly #mobile = inject(MobileService);
+  readonly #auth = inject(AuthService);
+  readonly #log = inject(BizyLogService);
+  readonly #toast = inject(BizyToastService);
+  readonly #database = inject(DatabaseService);
+  readonly #popup = inject(BizyPopupService);
+  readonly #usersService = inject(UsersService);
+  readonly #router = inject(BizyRouterService);
 
   async ngOnInit() {
     try {
-      if (this.mobile.isMobile()) {
-        await this.mobile.init();
-        this.mobile.backButton$.subscribe(() => {
-          if (ROOT_PATHS.includes(this.router.getURL())) {
-            this.mobile.exit();
+      if (this.#mobile.isMobile()) {
+        await this.#mobile.init();
+        this.#mobile.backButton$.subscribe(() => {
+          if (ROOT_PATHS.includes(this.#router.getURL())) {
+            this.#mobile.exit();
+          } else if (this.#popup.openedPopups() > 0) {
+            this.#popup.closeAll();
           } else {
-            this.router.goBack();
+            this.#router.goBack();
           }
         });
 
-        this.mobile.hideSplash();
+        this.#mobile.hideSplash();
       }
 
-      this.auth.signedIn$.subscribe(async signedIn => {
+      this.#auth.signedIn$.subscribe(async signedIn => {
         if (!signedIn) {
-          this.database.destroy();
-          this.router.goTo({ path: `/${PATH.AUTH}` });
+          this.#database.destroy();
+          this.#router.goTo({ path: `/${PATH.AUTH}` });
         } else {
           try {
-            await this.usersService.getCurrentUser();
+            await this.#usersService.getCurrentUser();
           } catch (error: any) {
-            this.log.error({
+            this.#log.error({
               fileName: 'app.component',
               functionName: 'getCurrentUser',
               param: error
             });
 
             if (error && error.message === ERROR.ITEM_NOT_FOUND) {
-              await this.usersService.postUser();
+              await this.#usersService.postUser();
             } else {
-              this.toast.danger();
+              this.#toast.danger();
             }
           }
 
-          if (this.router.getURL() === `/${PATH.AUTH}`) {
-            this.router.goTo({ path: `/${PATH.HOME}` });
+          if (this.#router.getURL() === `/${PATH.AUTH}`) {
+            this.#router.goTo({ path: `/${PATH.HOME}` });
           }
         }
       });
     } catch (error: any) {
-      this.log.error({
+      this.#log.error({
         fileName: 'app.component',
         functionName: 'ngOnInit',
         param: error
       });
-      this.toast.danger();
+      this.#toast.danger();
     }
   }
 }
