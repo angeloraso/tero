dc6be0cc47c1914e6c65dbac56ec4033e0410302
Angelo Raso <angeloraso6@gmail.com>
2025-03-24 19:53:19 -0300

    Adjust linter settings

---
+++ eslint.config.js
@@ -0,0 +1,63 @@
+import angular from '@angular-eslint/eslint-plugin';
+import angularTemplate from '@angular-eslint/eslint-plugin-template';
+import angularParser from '@angular-eslint/template-parser';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+
+const compat = new FlatCompat();
+
+export default [
+  js.configs.recommended,
+  ...compat.extends('plugin:@typescript-eslint/recommended'),
+  ...compat.extends('plugin:@angular-eslint/recommended'),
+  {
+    ignores: ['node_modules/', 'dist/', 'coverage/']
+  },
+  {
+    files: ['*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: ['tsconfig.json'],
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      '@angular-eslint': angular,
+      prettier,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          tabWidth: 2,
+          useTabs: false,
+          singleQuote: true,
+          semi: true,
+          bracketSpacing: true,
+          arrowParens: 'avoid',
+          trailingComma: 'es5',
+          bracketSameLine: true,
+          printWidth: 150,
+          endOfLine: 'auto'
+        },
+      ],
+    },
+  },
+  {
+    files: ['*.html'],
+    languageOptions: {
+      parser: angularParser,
+    },
+    plugins: {
+      '@angular-eslint/template': angularTemplate,
+    },
+    rules: {},
+  },
+];
