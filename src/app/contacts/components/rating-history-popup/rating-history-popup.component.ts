6db9141441964d55514a2262ef60bb2a47e94aa3
Angelo Raso <araso@anura.com.ar>
2025-06-28 13:51:03 -0300

    Run linter

--- src/app/contacts/components/rating-history-popup/rating-history-popup.component.ts
+++ src/app/contacts/components/rating-history-popup/rating-history-popup.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
 import { SharedModules } from '@app/shared';
 import { BIZY_TAG_TYPE, BizyPopupService } from '@bizy/core';
 import { DEFAULT_USER_PICTURE, IMG_PATH } from '@core/constants';
@@ -11,19 +11,19 @@ import { IContactRating } from '@core/model';
   imports: SharedModules
 })
 export class RatingHistoryPopupComponent implements OnInit {
+  readonly #popup = inject(BizyPopupService);
+
   rating: Array<IContactRating> = [];
 
   readonly BIZY_TAG_TYPE = BIZY_TAG_TYPE;
   readonly IMG_PATH = IMG_PATH;
   readonly DEFAULT_USER_PICTURE = DEFAULT_USER_PICTURE;
 
-  constructor(@Inject(BizyPopupService) private popup: BizyPopupService) {}
-
   ngOnInit() {
-    const data = this.popup.getData<{ rating: Array<IContactRating> }>();
+    const data = this.#popup.getData<{ rating: Array<IContactRating> }>();
 
     if (!data.rating || data.rating.length === 0) {
-      this.popup.close();
+      this.#popup.close();
       return;
     }
 
